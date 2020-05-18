fcfc80c0e178e44e842014ad6680b02b59ab05b1
liangweitian <liangweitian@kingsoft.com>
2020-05-18 09:02:42 +0800

    Crash due to missing Release

---
+++ qt5font_bug/font.cpp
@@ -0,0 +1,52 @@
+﻿#include <QtWidgets>
+#include <QtGui>
+#include <QtCore>
+
+
+class Window : public QWidget
+{
+public:
+	Window(QWidget* parent = nullptr)
+	{
+		fonts = QFontDatabase().families();
+
+		connect(&timer, &QTimer::timeout, this, &Window::updateFont);
+		timer.start(10);
+	}
+
+protected:
+	void paintEvent(QPaintEvent* event) override
+	{
+		QPainter painter(this);
+		QString str = QString::number(fonts.count());
+		painter.drawText(rect(), Qt::AlignCenter, str);
+	}
+
+private:
+	void updateFont()
+	{
+		if (fonts.empty())
+		{
+			timer.stop();
+			return;
+		}
+
+		QString font = fonts.takeFirst();
+		QString family = QFontInfo(font).family();
+		update();
+	}
+
+private:
+	QStringList fonts;
+	QTimer timer;
+};
+
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+
+	Window window;
+	window.show();
+
+	return app.exec();
+}
